30b7cd877435d1f6352f5a7af8667d0373a07820
Kevin <kevinshu.colorado@gmail.com>
2025-08-07 14:43:56 -0600

    Improved readability some more

--- findmyflags.c
+++ findmyflags.c
@@ -14,8 +14,7 @@ void flag_1(char* input) {
 }
 
 void flag_2(char* input) {
-	char to_decode[21] = "Q1NVLVNMRU4tMzQ4Mw==";
-	char* decoded = (char*)base64_decode(to_decode, 20, NULL);
+	char* decoded = (char*)base64_decode("Q1NVLVNMRU4tMzQ4Mw==", 20, NULL);
 	if (strcmp(input, decoded) != 0) fail();
 	free(decoded);
 }
@@ -58,86 +57,81 @@ void flag_4(char* input) {
 }
 
 void flag_5(char* input) {
-	char str[14] = {67,83,85,45,83,76,69,78,45,57,54,55,55,0};
-	if (strcmp(input, str) != 0) fail();
+	char flag[14] = {67,83,85,45,83,76,69,78,45,57,54,55,55,0};
+	if (strcmp(input, flag) != 0) fail();
 }
 
 void flag_6(char* input) {
-	char str[14] = {21,5,3,123,5,26,19,24,123,98,99,96,101,86};
-	for (int i = 0; i < sizeof(str); i++) {
-		str[i] ^= 86;
+	char to_xor[14] = {21,5,3,123,5,26,19,24,123,98,99,96,101,86};
+	for (int i = 0; i < sizeof(to_xor); i++) {
+		to_xor[i] ^= 86;
 	}
-	if (strcmp(input, str) != 0) fail();
+	if (strcmp(input, to_xor) != 0) fail();
 }
 
 void flag_7(char* input) {
-	char str[14] = {153,169,171,67,169,164,155,166,67,76,76,77,75,88};
-	for (int i = 0; i < sizeof(str); i++) {
-		str[i] -= 55;
-		str[i] ^= 33;
+	char to_subtract_and_xor[14] = {153,169,171,67,169,164,155,166,67,76,76,77,75,88};
+	for (int i = 0; i < sizeof(to_subtract_and_xor); i++) {
+		to_subtract_and_xor[i] -= 55;
+		to_subtract_and_xor[i] ^= 33;
 	}
-	if (strcmp(input, str) != 0) fail();
+	if (strcmp(input, to_subtract_and_xor) != 0) fail();
 }
 
 void secret_flag(char* input) {
-	uint8_t hashed[16];
-	md5String(input, hashed);
-	uint8_t correct[16] = {0x48,0xec,0x3a,0x12,0xf8,0x1e,0xb5,0x84,0x4b,0x53,0x6b,0xd2,0xe9,0xb8,0x7a,0x21};
-	if (memcmp(hashed, correct, 16) != 0) fail();
+	uint8_t input_hash[16];
+	md5String(input, input_hash);
+	uint8_t flag_hash[16] = {0x48,0xec,0x3a,0x12,0xf8,0x1e,0xb5,0x84,0x4b,0x53,0x6b,0xd2,0xe9,0xb8,0x7a,0x21};
+	if (memcmp(input_hash, flag_hash, 16) != 0) fail();
+}
+
+void get_user_input(char* output_buffer, size_t output_buffer_length) {
+	FILE* terminal_input = stdin;
+	fgets(output_buffer, output_buffer_length, terminal_input);
+	size_t newline_location_to_remove = strcspn(output_buffer, "\n");
+	output_buffer[newline_location_to_remove] = 0;
+	size_t carriage_return_location_to_remove = strcspn(output_buffer, "\n");
+	output_buffer[carriage_return_location_to_remove] = 0;
 }
 
 int main() {
-	char input[100];
+	char user_input[100];
+	const size_t max_user_input_length = sizeof(user_input);
 
 	printf("Input Flag 1: ");
-	fgets(input, sizeof(input), stdin);
-	input[strcspn(input, "\n")] = 0;
-	input[strcspn(input, "\r")] = 0;
-	flag_1(input);
+	get_user_input(user_input, max_user_input_length);
+	flag_1(user_input);
 
 	printf("Input Flag 2: ");
-	fgets(input, sizeof(input), stdin);
-	input[strcspn(input, "\n")] = 0;
-	input[strcspn(input, "\r")] = 0;
-	flag_2(input);
+	get_user_input(user_input, max_user_input_length);
+	flag_2(user_input);
 
-	char input_3[100];
+	char user_input_3[100];
+	const size_t max_user_input_3_length = sizeof(user_input_3);
 	printf("Input Flag 3: ");
-	fgets(input_3, sizeof(input_3), stdin);
-	input_3[strcspn(input_3, "\n")] = 0;
-	input_3[strcspn(input_3, "\r")] = 0;
-	flag_3(input_3);
+	get_user_input(user_input_3, max_user_input_3_length);
+	flag_3(user_input_3);
 
 	printf("Input Flag 4: ");
-	fgets(input, sizeof(input), stdin);
-	input[strcspn(input, "\n")] = 0;
-	input[strcspn(input, "\r")] = 0;
-	flag_4(input);
+	get_user_input(user_input, max_user_input_length);
+	flag_4(user_input);
 
 	printf("Input Flag 5: ");
-	fgets(input, sizeof(input), stdin);
-	input[strcspn(input, "\n")] = 0;
-	input[strcspn(input, "\r")] = 0;
-	flag_5(input);
+	get_user_input(user_input, max_user_input_length);
+	flag_5(user_input);
 
 	printf("Input Flag 6: ");
-	fgets(input, sizeof(input), stdin);
-	input[strcspn(input, "\n")] = 0;
-	input[strcspn(input, "\r")] = 0;
-	flag_6(input);
+	get_user_input(user_input, max_user_input_length);
+	flag_6(user_input);
 
 	printf("Input Flag 7: ");
-	fgets(input, sizeof(input), stdin);
-	input[strcspn(input, "\n")] = 0;
-	input[strcspn(input, "\r")] = 0;
-	flag_7(input);
+	get_user_input(user_input, max_user_input_length);
+	flag_7(user_input);
 
-	if (strlen(input_3) == 14 && input_3[13] == 'F') {
+	if (strlen(user_input_3) == 14 && user_input_3[13] == 'F') {
 		printf("\nYou've unlocked a secret :)\nInput Flag 8: ");
-		fgets(input, sizeof(input), stdin);
-		input[strcspn(input, "\n")] = 0;
-		input[strcspn(input, "\r")] = 0;
-		secret_flag(input);
+		get_user_input(user_input, max_user_input_length);
+		secret_flag(user_input);
 	}
 
 	printf("\nCongrats! You made it to the end!");
